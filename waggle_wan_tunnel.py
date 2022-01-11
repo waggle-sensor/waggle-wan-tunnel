ddc81e752eff624a8fd9b6150249e2854dcf52d3
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-01-11 15:16:24 -0600

    changed print to log

--- waggle_wan_tunnel.py
+++ waggle_wan_tunnel.py
@@ -1,33 +1,41 @@
 #!/usr/bin/env python3
+import argparse
 from configparser import ConfigParser
 from pathlib import Path
 from socket import gethostbyname
 import re
 import subprocess
-import time
+import logging
 
 
-def remove_stale_sshuttle_state():
-    print("removing stale sshuttle iptables state", flush=True)
-
-    # scan sshuttle rules and chains in nat table
+def remove_existing_sshuttle_state():
     output = subprocess.check_output(["iptables-save", "-t", "nat"]).decode()
     chains = re.findall(":(sshuttle-\d+)", output)
     rules = re.findall("-A(.*sshuttle.*)", output)
 
     for rule in rules:
-        print("removing rule", rule, flush=True)
+        logging.debug("removing rule: %s", rule)
         subprocess.check_call(["iptables", "-t", "nat", "-D"] + rule.split())
     
     for chain in chains:
-        print("removing chain", chain, flush=True)
+        logging.debug("removing chain: %s", chain)
         subprocess.check_call(["iptables", "-t", "nat", "-X", chain])
 
-    print("done", flush=True)
+
+def run(cmd):
+    logging.debug("run %s", " \\\n\t".join(map(repr, cmd)))
+    subprocess.run(cmd, check=True)
 
 
 def main():
-    remove_stale_sshuttle_state()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", action="store_true", help="enable verbose logging")
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        level=logging.DEBUG if args.debug else logging.INFO,
+        format="%(asctime)s %(message)s",
+        datefmt="%Y/%m/%d %H:%M:%S")
 
     # get node and reverse tunnel config 
     node_id = Path("/etc/waggle/node-id").read_text().strip()
@@ -45,7 +53,11 @@ def main():
     bk_ip = gethostbyname(bk_host)
     bk_user = f"node-{node_id}"
 
-    sshuttle_cmd = [
+    logging.info("removing any existing sshuttle state...")
+    remove_existing_sshuttle_state()
+
+    logging.info("running sshuttle...")
+    run([
         "sshuttle",
         "-e", f"ssh {ssh_options} -o ServerAliveInterval={ssh_keepalive_interval} -o ServerAliveCountMax={ssh_keepalive_count} -i {bk_key}",
         "-x", f"{bk_ip}/16",  # tunnel cidr
@@ -54,9 +66,7 @@ def main():
         "-x", "10.43.0.0/16",  # k3s svc cidr
         "-r", f"{bk_user}@{bk_host}:{bk_port}",
         "0/0",
-    ]
-    print("running", " \\\n\t".join(map(repr, sshuttle_cmd)), flush=True)
-    subprocess.run(sshuttle_cmd, check=True)
+    ])
 
 
 if __name__ == "__main__":
