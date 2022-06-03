d0b0675b865fd02456f2ac28c4bd52b49878ecf7
Joe Swantek <joseph.swantek@northwestern.edu>
2022-06-03 14:34:28 -0500

    SAGE-1354: remove 'Sage' reference and use latest deb-builder

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@
 docker run --rm \
   -e NAME="waggle-wan-tunnel" \
   -e DESCRIPTION="Service which managers WAN traffic tunnel." \
-  -e "MAINTAINER=sagecontinuum.org" \
   -v "$PWD:/repo" \
-  waggle/waggle-deb-builder:0.2.0
+  waggle/waggle-deb-builder:latest
